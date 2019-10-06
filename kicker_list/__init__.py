8105093163aecd3d41bb7e2a27930a9aeccae844
Gideon <gideons@web.de>
2019-10-06 19:01:17 +0200

    initial commit

---
+++ kicker_list/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '0798aaa973a4f300d8a2277a9db963f2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from kicker_list import routes
---
+++ run.py
@@ -0,0 +1,4 @@
+from kicker_list import app
+
+if __name__ == '__main__':
+    app.run(debug = True)
